dd68828b91cf713543d3d59ebd92a247f2ef576f
unknown <jenei.viktor@gmail.com>
2025-04-13 18:47:29 +0200

    New background theme color

--- app/wake_on_lan.py
+++ app/wake_on_lan.py
@@ -10,7 +10,7 @@ html_page = """
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="manifest" href="{{ url_for('static', filename='manifest.json') }}">
-    <meta name="theme-color" content="#007BFF">
+    <meta name="theme-color" content="#f4f4f4">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
